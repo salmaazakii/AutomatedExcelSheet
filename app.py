54e09b4f23121974df3388654bf3189c9e019d17
salmaazakii <salmazaki700@gmail.com>
2023-12-08 17:04:07 +0200

    adding chart to Excel sheet

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import openpyxl as excel
+from openpyxl.chart import BarChart, Reference
 
 workbook = excel.load_workbook('transactions.xlsx')
 sheet = workbook['Sheet1']
@@ -11,5 +12,10 @@ for row in range(2, sheet.max_row+1):
     corrected_value = cell.value * 0.9
     corrected_value_cell = sheet.cell(row, 4)
     corrected_value_cell.value = corrected_value
+
+values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
 
 workbook.save('transactions2.xlsx')
